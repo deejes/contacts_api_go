4a08adb044f048043791ae9024b3e47c19394b75
deejes <j.devansh29@gmail.com>
2018-01-16 17:45:14 -0800

    complete delete person function

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
     "log"
     "net/http"
     "encoding/json"
-    //"fmt"
   )
 
 func main(){
@@ -29,8 +28,24 @@ func GetPerson(w http.ResponseWriter, r *http.Request){
       for _, item := range people {
       if item.ID == params["id"]{json.NewEncoder(w).Encode(item)}}
   }
-func CreatePerson(w http.ResponseWriter, r *http.Request){}
-func DeletePerson(w http.ResponseWriter, r *http.Request){}
+func CreatePerson(w http.ResponseWriter, r *http.Request){
+    params := mux.Vars(r)
+    var person Person
+    _ = json.NewDecoder(r.Body).Decode(&person)
+    person.ID = params["id"]
+    people = append(people, person)
+    json.NewEncoder(w).Encode(people)
+     }
+
+func DeletePerson(w http.ResponseWriter, r *http.Request){
+    params := mux.Vars(r)
+    for index,item := range people{
+        if item.ID == params["id"] {
+          people = append(people[:index], people[index+1:]...)
+          break
+        }
+    }
+}
 
 
 type Person struct{
